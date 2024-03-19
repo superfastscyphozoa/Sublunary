eb6cd0d75e5b0c959a93b49fcb206df229a587d9
superfastscyphozoa <mitchellelhage@gmail.com>
2024-03-19 21:53:26 +1100

    i hate these goddamn crows

--- src/main/java/net/superfastscyphozoa/sublunary/world/gen/SublunaryEntityGeneration.java
+++ src/main/java/net/superfastscyphozoa/sublunary/world/gen/SublunaryEntityGeneration.java
@@ -16,15 +16,26 @@ public class SublunaryEntityGeneration {
     public static void generateEntities(){
 
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
-                SpawnGroup.CREATURE, EntityType.MULE, 16, 1, 4);
-
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
                 SpawnGroup.CREATURE, EntityType.RABBIT, 12, 3, 5);
 
         BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
                 SpawnGroup.CREATURE, EntityType.FOX, 14, 2, 3);
 
-        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.PLAINS, BiomeKeys.FOREST),
-                SpawnGroup.CREATURE, RegisterEntities.CROW, 16, 3, 5);
+        BiomeModifications.addSpawn(BiomeSelectors.includeByKey(BiomeKeys.PLAINS),
+                SpawnGroup.CREATURE, RegisterEntities.CROW, 12, 3, 5);
+
+        BiomeModifications.create(new Identifier(Sublunary.MOD_ID,"replace_forest_creatures")).add
+                (ModificationPhase.REPLACEMENTS, BiomeSelectors.includeByKey(BiomeKeys.FOREST), biomeModificationContext -> {
+
+                    if (biomeModificationContext.getSpawnSettings().removeSpawnsOfEntityType(EntityType.COW)){
+                        biomeModificationContext.getSpawnSettings()
+                                .addSpawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(EntityType.MULE, 16, 1, 4));
+                    }
+
+                    if (biomeModificationContext.getSpawnSettings().removeSpawnsOfEntityType(EntityType.CHICKEN)){
+                        biomeModificationContext.getSpawnSettings()
+                                .addSpawn(SpawnGroup.CREATURE, new SpawnSettings.SpawnEntry(RegisterEntities.CROW, 10, 3, 5));
+                    }
+                });
     }
 }
